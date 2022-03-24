49352f28d2a2bb92cc7c5a83e3d04fbaaaf30e16
Viktor <Rickeygame23@gmail.com>
2022-03-24 08:01:00 +0200

    date

--- script.js
+++ script.js
@@ -1,34 +1,20 @@
-const btn = document.querySelector('.btn');
-let timerId,
-    i = 0;
+'user strict'
 
-function myAnimation() {
-    const elem = document.querySelector('.box');
-    let pos = 0;
-    
-    const id = setInterval(frame, 10);
-    function frame() {
-        if (pos == 300) {
-            clearInterval(id);
-        } else {
-            pos++;
-            elem.style.top = pos + 'px';
-            elem.style.left = pos + 'px';                
-        }
-    }
-}    
+const now = new Date();
 
-btn.addEventListener('click', myAnimation);
+// console.log(now.setHours(18));
+// console.log(now);
+// console.log(now.getDay());
+// console.log(now.getHours());
+// console.log(now.getUTCHours());
+// console.log(now.getTime());
 
-// function logger() {
-//     if (i == 3) {
-//         clearInterval(timerId)
-//     }
-//     console.log('hello man');
-//     i++;
-// }
+let start = new Date();
 
-// let id = setTimeout(function log(){
-//     console.log('Hello');
-//     id = setTimeout(log, 2000);
-// }, 500);
+for (let i = 0; i < 100000; i++){
+    let some = i ** 3;
+}
+
+let end = new Date();
+
+console.log(`Цикл ${end - start}`);
